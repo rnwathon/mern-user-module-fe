e1b945128753a749cad79a834cd3bf06d2fa4a16
Rakhmat Nashrul Wathon <rakhmatnashrul@gmail.com>
2020-05-31 21:24:14 +0700

    Intergrate the route with the components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
+import User from './modules/User/User';
+import AddUser from './modules/User/AddUser';
+import Login from './modules/Login/Login';
+
+import PrivateRoute from './routes/PrivateRoute'
+
+
 function App() {
   return (
     <React.Fragment>
       <Router>
         <Switch>
+          <PrivateRoute path="/" component={User} exact/>
+          <PrivateRoute path="/addUser" component={AddUser} exact />
+          <Route path="/login" component={Login} exact/>
           <Route component={() => "404 Not Found"} />
         </Switch>
       </Router>
